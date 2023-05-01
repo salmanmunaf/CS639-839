a454e26a7c27e5254db4bb0f139d2cca93d7544f
Salman Munaf <salmanmunaf96@gmail.com>
2023-05-01 01:44:22 -0500

    the deployment script is working and tested

--- project/deployer.js
+++ project/deployer.js
@@ -1,26 +1,46 @@
-const Web3 = require('web3');
-const contract = require('./contract.json');
+const Web3 = require("web3");
 
-// Connect to the Goerli testnet
-const provider = new Web3.providers.HttpProvider(`https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`);
-const web3 = new Web3(provider);
+// Loading the contract ABI and Bytecode
+// (the results of a previous compilation step)
+const fs = require("fs");
+const { abi, bytecode } = JSON.parse(fs.readFileSync("contract.json"))['roulette.vy'];
+const BIDDING_TIME = 20; // set bidding time
 
-// Create an instance of your contract
-const myContract = new web3.eth.Contract(contract['roulette.vy'].abi);
+async function main() {
+  // Configuring the connection to an Ethereum node
+  const network = process.env.ETHEREUM_NETWORK;
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+    )
+  );
+  // Creating a signing account from a private key
+  const signer = web3.eth.accounts.privateKeyToAccount(
+    process.env.SIGNER_PRIVATE_KEY
+  );
+  web3.eth.accounts.wallet.add(signer);
 
-// Deploy the contract
-myContract.deploy({
-  data: contract['roulette.vy'].bytecode,
-  arguments: [20] // Set the bidding time accordingly
-})
-.send({
-  from: process.env.WALLET_ADDRESS,
-  gas: '900000',
-  gasPrice: web3.utils.toWei('20', 'gwei') // Set a gas price to ensure timely processing of the transaction
-})
-.on('receipt', (receipt) => {
-  console.log('Contract deployed at:', receipt.contractAddress);
-})
-.on('error', (error) => {
-  console.error('Error deploying contract:', error);
-});
+  // Using the signing account to deploy the contract
+  const contract = new web3.eth.Contract(abi);
+  contract.options.data = bytecode;
+  const deployTx = contract.deploy({
+    arguments: [BIDDING_TIME]
+  });
+  const deployedContract = await deployTx
+    .send({
+      from: signer.address,
+      gas: await deployTx.estimateGas(),
+    })
+    .once("transactionHash", (txhash) => {
+      console.log(`Mining deployment transaction ...`);
+      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+    });
+  // The contract is now deployed on chain!
+  console.log(`Contract deployed at ${deployedContract.options.address}`);
+  console.log(
+    `Add DEMO_CONTRACT to the.env file to store the contract address: ${deployedContract.options.address}`
+  );
+}
+
+require("dotenv").config();
+main();
