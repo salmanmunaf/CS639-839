d25f2e3974944196bd3e2a4a01dcc858843143e9
Salman Munaf <salmanmunaf96@gmail.com>
2023-05-10 00:48:17 -0500

    updated comments

--- project/VRFConsumer.sol
+++ project/VRFConsumer.sol
@@ -12,10 +12,10 @@ contract VRFConsumer is VRFConsumerBase {
     /**
      * Constructor inherits VRFConsumerBase
      *
-     * Network: Kovan
-     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
-     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
-     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     * Network
+     * Chainlink VRF Coordinator address
+     * LINK token address
+     * Key Hash
      */
     constructor(
         bytes32 _keyhash,
